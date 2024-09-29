d3e0d7ea0922e4cd524251de44d94e0c1d681efb
Grayson Nocera <gnocera@purdue.edu>
2024-09-29 03:12:46 +0000

    copy things over from lab2

--- part3_pointer_aware_data_flow_analysis/DivZero/src/DivZeroAnalysis.cpp
+++ part3_pointer_aware_data_flow_analysis/DivZero/src/DivZeroAnalysis.cpp
@@ -1,77 +1,432 @@
 #include "DivZeroAnalysis.h"
 
-namespace dataflow {
+namespace dataflow
+{
 
-//===----------------------------------------------------------------------===//
-// Dataflow Analysis Implementation
-//===----------------------------------------------------------------------===//
+  //===----------------------------------------------------------------------===//
+  // Dataflow Analysis Implementation
+  //===----------------------------------------------------------------------===//
 
-/**
- * Implement your data-flow analysis.
- * 1. Define "flowIn" that joins the memory set of all incoming flows
- * 2. Define "transfer" that computes the semantics of each instruction.
- * 3. Define "flowOut" that flows the memory set to all outgoing flows
- * 4. Define "doAnalysis" that stores your results in "InMap" and "OutMap".
- * 5. Define "check" that checks if a given instruction is erroneous or not.
- */
+  /**
+   * Implement your data-flow analysis.
+   * 1. Define "flowIn" that joins the memory set of all incoming flows
+   * 2. Define "transfer" that computes the semantics of each instruction.
+   * 3. Define "flowOut" that flows the memory set to all outgoing flows
+   * 4. Define "doAnalysis" that stores your results in "InMap" and "OutMap".
+   * 5. Define "check" that checks if a given instruction is erroneous or not.
+   */
 
-
-// define the following functions if needed (not compulsory to do so)
-Memory* join(Memory *M1, Memory *M2) {
-  Memory* Result = new Memory();
-  /* Add your code here */
-
-  /* Result will be the union of memories M1 and M2 */
-
-  return Result;
-}
-
-bool equal(Memory *M1, Memory *M2) {
-  /* Add your code here */
-  /* Return true if the two memories M1 and M2 are equal */
-}
-
-
-void DivZeroAnalysis::flowIn(Instruction *I, Memory *In) {
-  /* Add your code here */
-}
-
-
-void DivZeroAnalysis::transfer(Instruction *I, const Memory *In, Memory *NOut,
-                               PointerAnalysis *PA,
-                               SetVector<Value *> PointerSet) {
-  /* Add your code here */
-}
-
-void DivZeroAnalysis::flowOut(Instruction *I, Memory *Pre, Memory *Post,  SetVector <Instruction *> &WorkSet) {
-  /* Add your code here */
-}
-
-void DivZeroAnalysis::doAnalysis(Function &F, PointerAnalysis *PA) {
-  /* Add your code here */
-  SetVector<Instruction *> WorkSet;
-  SetVector<Value *> PointerSet;
-  for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
-    WorkSet.insert(&(*I));
-    PointerSet.insert(&(*I));
+  void printMemory(Memory *m1)
+  {
+    outs() << "Printing Memory:\n";
+    for (auto pair = m1->begin(); pair != m1->end(); ++pair)
+    {
+      char str[1024];
+      sprintf(str, "\t%20s -> %d\n", pair->first.c_str(), pair->second->Value);
+      outs() << str;
+    }
+    outs() << "\n";
   }
 
-  /* Add your code here */
-  /* Basic Workflow-
-       Visit instruction in WorkSet
-       For each visited instruction I, construct its In memory by joining all memory sets of incoming flows (predecessors of I)
-       Based on the type of instruction I and the In memory, populate the NOut memory. Take the pointer analysis into consideration for this step
-       Based on the previous Out memory and the current Out memory, check if there is a difference between the two and
-         flow the memory set appropriately to all successors of I and update WorkSet accordingly
-  */ 
-}
+  Memory *join(Memory *M1, Memory *M2)
+  {
+    Memory *Result = new Memory();
 
-bool DivZeroAnalysis::check(Instruction *I) {
-  /* Add your code here */
-  return false;
-}
+    for (auto pair = M1->begin(); pair != M1->end(); ++pair)
+    {
+      Result->insert({pair->first, pair->second});
+    }
 
-char DivZeroAnalysis::ID = 1;
-static RegisterPass<DivZeroAnalysis> X("DivZero", "Divide-by-zero Analysis",
-                                       false, false);
+    for (auto pair = M2->begin(); pair != M2->end(); ++pair)
+    {
+      if (Result->find(pair->first) == Result->end())
+      {
+        // cannot find M2 key in M1, so we call it Uninit in M1
+        // Domain *d = Domain::join(new Domain(Domain::Uninit), pair->second);
+        Result->insert({pair->first, pair->second});
+      }
+      else
+      {
+        // it is in both hashmaps
+        Domain *m1d = Result->at(pair->first);
+        if (m1d->Value == Domain::Uninit)
+        {
+          Result->insert({pair->first, pair->second});
+        }
+        else
+        {
+
+          Domain *d = Domain::join(Result->at(pair->first), pair->second);
+          Result->insert({pair->first, d});
+        }
+      }
+    }
+
+    return Result;
+  }
+
+  bool equal(Memory *M1, Memory *M2)
+  {
+    /* Add your code here */
+    /* Return true if the two memories M1 and M2 are equal */
+    // merge m2 into m1, then check if m1 equals the previous m1
+
+    if (M1->size() != M2->size())
+    {
+      return false;
+    }
+    for (auto m1 = M1->begin(); m1 != M1->end(); ++m1)
+    {
+      if (M2->find(m1->first) == M2->end())
+      {
+        // couldn't find M1 key in M2, thus return false
+        return false;
+      }
+      if (M2->at(m1->first)->Value != M1->at(m1->first)->Value)
+      {
+        // if the values here are not equal, return false
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  void DivZeroAnalysis::flowIn(Instruction *I, Memory *In)
+  {
+    // joining all OUT sets of incoming flows and saving the result in the In set
+    std::vector<Instruction *> predecessors = getPredecessors(I);
+    Memory *joined = NULL;
+    for (Instruction *predecessor : predecessors)
+    {
+      // populate In, which is a hash table {string : Domain *}
+      Memory *outN = OutMap[predecessor];
+      printMemory(outN);
+      if (!joined)
+      {
+        joined = outN;
+      }
+      joined = join(joined, outN);
+    }
+    if (!joined)
+    {
+      return;
+    }
+
+    for (auto pair = joined->begin(); pair != joined->end(); ++pair)
+    {
+      In->insert({pair->first, pair->second});
+    }
+  }
+
+  void DivZeroAnalysis::transfer(Instruction *I, const Memory *In, Memory *NOut,
+                                 PointerAnalysis *PA,
+                                 SetVector<Value *> PointerSet)
+  {
+    /* Add your code here */
+    for (auto in = In->begin(); in != In->end(); in++)
+    {
+      NOut->insert({in->first, in->second});
+    }
+
+    if (BinaryOperator *BO = dyn_cast<BinaryOperator>(I))
+    {
+      outs() << "Binary operator\n";
+      Value *op1 = BO->getOperand(0);
+      Value *op2 = BO->getOperand(1);
+      Domain *d1 = new Domain(Domain::Uninit);
+      Domain *d2 = new Domain(Domain::Uninit);
+
+      // rational: if we find the variable in incoming hashmap, use it,
+      // otherwise, if we see a constant, set that domain explicitly
+      ConstantInt *int1 = nullptr;
+      ConstantInt *int2 = nullptr;
+      if (In->find(variable(op1)) == In->end())
+      {
+        if (ConstantInt *i = dyn_cast<ConstantInt>(op1))
+        {
+          d1->Value = i->isZero() ? Domain::Zero : Domain::NonZero;
+          int1 = i;
+        }
+      }
+      else
+      {
+        d1 = In->at(variable(op1));
+      }
+
+      if (In->find(variable(op2)) == In->end())
+      {
+        if (ConstantInt *i = dyn_cast<ConstantInt>(op2))
+        {
+          d2->Value = i->isZero() ? Domain::Zero : Domain::NonZero;
+          int2 = i;
+        }
+      }
+      else
+      {
+        d2 = In->at(variable(op2));
+      }
+      outs() << "Parsed d1, d2 = " << d1->Value << " " << d2->Value << "\n";
+
+      if (int1 && int2)
+      {
+        APInt a1 = int1->getValue();
+        APInt a2 = int2->getValue();
+        if ((a1.isStrictlyPositive() && a2.isStrictlyPositive()) || (a1.isNegative() && a2.isNegative()))
+        {
+          // this is to induce a NonZero output
+          d1->Value = Domain::Zero;
+          d2->Value = Domain::NonZero;
+        }
+      }
+
+      switch (BO->getOpcode())
+      {
+      case Instruction::Add:
+        outs() << "adding " << d1->Value << " " << d2->Value << "\n";
+        NOut->insert({variable(I), Domain::add(d1, d2)});
+        break;
+      case Instruction::Sub:
+        NOut->insert({variable(I), Domain::sub(d1, d2)});
+        break;
+      case Instruction::Mul:
+        NOut->insert({variable(I), Domain::mul(d1, d2)});
+        break;
+      case Instruction::SDiv:
+      case Instruction::UDiv:
+        NOut->insert({variable(I), Domain::div(d1, d2)});
+        break;
+      }
+      outs() << "Nout for add " << NOut->at(variable(I)) << "\n";
+    }
+    else if (CastInst *CastI = dyn_cast<CastInst>(I))
+    {
+      outs() << "Cast int\n";
+      // not worrying about overflow and underflow here
+      Value *op1 = CastI->getOperand(0);
+      Domain *d = new Domain(Domain::Uninit);
+      if (In->find(variable(op1)) == In->end())
+      {
+        if (ConstantInt *i = dyn_cast<ConstantInt>(op1))
+        {
+          d->Value = i->isZero() ? Domain::Zero : Domain::NonZero;
+        }
+      }
+      else
+      {
+        d = In->at(variable(op1));
+      }
+      NOut->insert({variable(I), d});
+    }
+    else if (CmpInst *CmpI = dyn_cast<CmpInst>(I))
+    {
+      outs() << "compare inst\n";
+      Value *op1 = CmpI->getOperand(0);
+      Value *op2 = CmpI->getOperand(1);
+      Domain *d1 = new Domain(Domain::Uninit);
+      Domain *d2 = new Domain(Domain::Uninit);
+      Domain *d3 = new Domain(Domain::Uninit);
+
+      // rational: if we find the variable in incoming hashmap, use it,
+      // otherwise, if we see a constant, set that domain explicitly
+      if (In->find(variable(op1)) == In->end())
+      {
+        if (ConstantInt *i = dyn_cast<ConstantInt>(op1))
+        {
+          d1->Value = i->isZero() ? Domain::Zero : Domain::NonZero;
+        }
+      }
+      else
+      {
+        d1 = In->at(variable(op1));
+      }
+
+      if (In->find(variable(op2)) == In->end())
+      {
+        if (ConstantInt *i = dyn_cast<ConstantInt>(op2))
+        {
+          d2->Value = i->isZero() ? Domain::Zero : Domain::NonZero;
+        }
+      }
+      else
+      {
+        d2 = In->at(variable(op2));
+      }
+      outs() << "got d2: " << d2->Value << " and d3: " << d3->Value << "\n";
+
+      if (d1->Value == Domain::MaybeZero || d2->Value == Domain::MaybeZero)
+      {
+        d3->Value = Domain::MaybeZero;
+      }
+      else if (d1->Value == Domain::Uninit || d2->Value == Domain::Uninit)
+      {
+        d3->Value = Domain::Uninit;
+      }
+      else
+      {
+        // at this point, we know that neither are MaybeZero or Uninit
+        switch (CmpI->getPredicate())
+        {
+        case CmpInst::ICMP_EQ:
+          if (d1->Value == Domain::Zero && d2->Value == Domain::Zero)
+          {
+            d3->Value = Domain::NonZero;
+          }
+          else if (d1->Value == Domain::NonZero && d2->Value == Domain::NonZero)
+          {
+            d3->Value = Domain::MaybeZero;
+          }
+          else
+          {
+            d3->Value = Domain::Zero;
+          }
+          break;
+        case CmpInst::ICMP_NE:
+          if (d1->Value == Domain::Zero && d2->Value == Domain::Zero)
+          {
+            d3->Value = Domain::Zero;
+          }
+          else if (d1->Value == Domain::NonZero && d2->Value == Domain::NonZero)
+          {
+            d3->Value = Domain::MaybeZero;
+          }
+          else
+          {
+            d3->Value = Domain::NonZero;
+          }
+        case CmpInst::ICMP_SGE:
+        case CmpInst::ICMP_UGE:
+        case CmpInst::ICMP_SLE:
+        case CmpInst::ICMP_ULE:
+          if (d1->Value == Domain::Zero && d2->Value == Domain::Zero)
+          {
+            d3->Value = Domain::NonZero;
+          }
+          else
+          {
+            d3->Value = Domain::MaybeZero;
+          }
+          break;
+        case CmpInst::ICMP_SGT:
+        case CmpInst::ICMP_UGT:
+        case CmpInst::ICMP_SLT:
+        case CmpInst::ICMP_ULT:
+          if (d1->Value == Domain::Zero && d2->Value == Domain::Zero)
+          {
+            d3->Value = Domain::Zero;
+          }
+          else
+          {
+            d3->Value = Domain::MaybeZero;
+          }
+          break;
+        }
+      }
+      NOut->insert({variable(I), d3});
+    }
+    else if (BranchInst *BI = dyn_cast<BranchInst>(I))
+    {
+      outs() << "branch inst\n";
+      // nothing to do here? we don't get any new variables or anything
+    }
+    else if (isInput(I))
+    {
+      outs() << "getchar()\n";
+      // getchar() returns int -> maybeZero
+      Domain *d = new Domain(Domain::MaybeZero);
+      outs() << "d->Value" << d->Value << "\n";
+      NOut->insert({variable(I), d});
+    }
+  }
+
+  void DivZeroAnalysis::flowOut(Instruction *I, Memory *Pre, Memory *Post, SetVector<Instruction *> &WorkSet)
+  {
+    printMemory(Pre);
+    printMemory(Post);
+    bool isEqual = equal(Pre, Post);
+    if (!isEqual)
+    {
+      // add it back to worker set for processing
+      WorkSet.insert(I);
+    }
+
+    // I'm confused by this
+    for (auto pair = Post->begin(); pair != Post->end(); ++pair)
+    {
+      OutMap[I]->insert({pair->first, pair->second});
+    }
+  }
+
+  void DivZeroAnalysis::doAnalysis(Function &F, PointerAnalysis *PA)
+  {
+    /* Add your code here */
+    SetVector<Instruction *> WorkSet;
+    SetVector<Value *> PointerSet;
+    for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I)
+    {
+      WorkSet.insert(&(*I));
+      PointerSet.insert(&(*I));
+    }
+
+    /* Add your code here */
+    /* Basic Workflow-
+         Visit instruction in WorkSet
+         For each visited instruction I, construct its In memory by joining all memory sets of incoming flows (predecessors of I)
+         Based on the type of instruction I and the In memory, populate the NOut memory. Take the pointer analysis into consideration for this step
+         Based on the previous Out memory and the current Out memory, check if there is a difference between the two and
+           flow the memory set appropriately to all successors of I and update WorkSet accordingly
+    */
+  }
+
+  bool DivZeroAnalysis::check(Instruction *I)
+  {
+    Memory *mem = InMap[I];
+    if (BinaryOperator *BO = dyn_cast<BinaryOperator>(I))
+    {
+      switch (BO->getOpcode())
+      {
+      case Instruction::SDiv:
+      case Instruction::UDiv:
+        outs() << "checking for errors \n";
+        Value *op1 = BO->getOperand(0);
+        Value *op2 = BO->getOperand(1);
+        Domain *d1 = new Domain(Domain::Uninit);
+        Domain *d2 = new Domain(Domain::Uninit);
+        if (mem->find(variable(op1)) == mem->end())
+        {
+          if (ConstantInt *i = dyn_cast<ConstantInt>(op1))
+          {
+            d1->Value = i->isZero() ? Domain::Zero : Domain::NonZero;
+          }
+        }
+        else
+        {
+          d1 = mem->at(variable(op1));
+        }
+        if (mem->find(variable(op2)) == mem->end())
+        {
+          if (ConstantInt *i = dyn_cast<ConstantInt>(op2))
+          {
+            d2->Value = i->isZero() ? Domain::Zero : Domain::NonZero;
+          }
+        }
+        else
+        {
+          d2 = mem->at(variable(op2));
+        }
+
+        // actual check for division by 0 occurs here
+        if (d2->Value == Domain::Zero || d2->Value == Domain::MaybeZero)
+        {
+          outs() << "DIVISION BY 0\n";
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  char DivZeroAnalysis::ID = 1;
+  static RegisterPass<DivZeroAnalysis> X("DivZero", "Divide-by-zero Analysis",
+                                         false, false);
 } // namespace dataflow
--- part3_pointer_aware_data_flow_analysis/DivZero/src/Domain.cpp
+++ part3_pointer_aware_data_flow_analysis/DivZero/src/Domain.cpp
@@ -5,3 +5,132 @@
 //===----------------------------------------------------------------------===//
 
 /* Add your code here */
+
+dataflow::Domain *dataflow::Domain::add(Domain *E1, Domain *E2)
+{
+    dataflow::Domain::Element e1 = E1->Value;
+    dataflow::Domain::Element e2 = E2->Value;
+    dataflow::Domain * e3 = new dataflow::Domain();
+
+    if (e1 == dataflow::Domain::Uninit || e2 == dataflow::Domain::Uninit || e1 == dataflow::Domain::MaybeZero || e2 == dataflow::Domain::MaybeZero)
+    {
+        e3->Value = dataflow::Domain::MaybeZero;
+    }
+    else if (e1 == dataflow::Domain::Zero && e2 == dataflow::Domain::Zero)
+    {
+        e3->Value = dataflow::Domain::Zero;
+    }
+    else if (e1 == dataflow::Domain::NonZero && e2 == dataflow::Domain::NonZero)
+    {
+        e3->Value = dataflow::Domain::MaybeZero;
+    }
+    else // at this point, we know one is nonzero and one is zero
+    {
+        e3->Value = dataflow::Domain::NonZero;
+    }
+
+    outs() << "e1, e2 = e3 -> " << e1 << ", " << e2 << " = " << e3->Value << "\n";
+
+    return e3;
+}
+
+dataflow::Domain *dataflow::Domain::sub(Domain *E1, Domain *E2)
+{
+    // if both 0, result is 0
+    // if both nonzero, result is maybeZero
+    // if they are different, result is nonZero
+    // I think sub same as add
+
+    return dataflow::Domain::add(E1, E2);
+}
+
+dataflow::Domain *dataflow::Domain::mul(Domain *E1, Domain *E2)
+{
+    dataflow::Domain::Element e1 = E1->Value;
+    dataflow::Domain::Element e2 = E2->Value;
+    dataflow::Domain *e3 = new dataflow::Domain();
+
+    // if both 0 => 0
+    // if neither 0 => nonZero
+    // if either 0 => 0
+
+    if (e1 == dataflow::Domain::Uninit || e2 == dataflow::Domain::Uninit || e1 == dataflow::Domain::MaybeZero || e2 == dataflow::Domain::MaybeZero)
+    {
+        e3->Value = dataflow::Domain::MaybeZero;
+    }
+    else if (e1 == dataflow::Domain::Zero && e2 == dataflow::Domain::Zero)
+    {
+        e3->Value = dataflow::Domain::Zero;
+    }
+    else if (e1 == dataflow::Domain::NonZero && e2 == dataflow::Domain::NonZero)
+    {
+        e3->Value = dataflow::Domain::NonZero;
+    }
+    else // at this point, we know one is nonzero and one is zero
+    {
+        e3->Value = dataflow::Domain::Zero;
+    }
+
+    return e3;
+}
+
+dataflow::Domain *dataflow::Domain::div(Domain *E1, Domain *E2)
+{
+    dataflow::Domain::Element e1 = E1->Value;
+    dataflow::Domain::Element e2 = E2->Value;
+    dataflow::Domain *e3 = new dataflow::Domain();
+
+    // if both are 0, undefined
+    // if either are 0, undefined
+    // if neither are 0, nonZero
+
+    if (e1 == dataflow::Domain::Uninit || e2 == dataflow::Domain::Uninit || e1 == dataflow::Domain::MaybeZero || e2 == dataflow::Domain::MaybeZero)
+    {
+        e3->Value = dataflow::Domain::MaybeZero;
+    }
+    else if (e2 == dataflow::Domain::Zero)
+    {
+        // undefined behavior, division by 0 error
+        e3->Value = dataflow::Domain::MaybeZero;
+    }
+    else if (e1 == dataflow::Domain::Zero)
+    {
+        // at this point, we know e2 is not 0, e1 is 0, thus, 0 / x = 0
+        e3->Value = dataflow::Domain::Zero;
+    }
+
+    return e3;
+}
+
+dataflow::Domain *dataflow::Domain::join(Domain *E1, Domain *E2)
+{
+    dataflow::Domain::Element e1 = E1->Value;
+    dataflow::Domain::Element e2 = E2->Value;
+    dataflow::Domain *e3 = new dataflow::Domain();
+
+    // if they are the same, it's just that
+    // if one is 0 and one is nonzero, maybezero
+
+    if (e1 == dataflow::Domain::Uninit) {
+        e3->Value = e2;
+    } else if (e2 == dataflow::Domain::Uninit) {
+        e3->Value = e1;
+    } else if (e1 == dataflow::Domain::MaybeZero || e2 == dataflow::Domain::MaybeZero) {
+        e3->Value = dataflow::Domain::MaybeZero;
+    } else if (e1 == e2) {
+        e3->Value = e1;
+    } else {
+        e3->Value = dataflow::Domain::MaybeZero;
+    }
+
+    return e3;
+}
+
+dataflow::Domain::Domain(Element V)
+{
+    Value = V;
+}
+
+dataflow::Domain::Domain()
+{
+}
