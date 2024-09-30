fc8955207f52a98ebe0f31c926a2a014d8d0d720
Grayson Nocera <gnocera@purdue.edu>
2024-09-30 13:26:32 +0000

    i mean i can't check it because i'm on a mac but here is part 4

--- part4_instrumentation/DivZeroInstrument/src/Instrument.cpp
+++ part4_instrumentation/DivZeroInstrument/src/Instrument.cpp
@@ -2,32 +2,91 @@
 
 using namespace llvm;
 
-namespace instrument {
+namespace instrument
+{
 
-static const char *SanitizerFunctionName = "__sanitize__";
-static const char *CoverageFunctionName = "__coverage__";
+  static const char *SanitizerFunctionName = "__sanitize__";
+  static const char *CoverageFunctionName = "__coverage__";
 
-/*
- * Implement divide-by-zero sanitizer.
- */
-void instrumentSanitize(Module *M, Function &F, Instruction &I) {
-  /* Add you code here */
-}
+  /*
+   * Implement divide-by-zero sanitizer.
+   */
+  void instrumentSanitize(Module *M, Function &F, Instruction &I)
+  {
+    /* Add you code here */
+    // insert a sanitize check for a supplied instruction
 
-/*
- * Implement code coverage instrumentation.
- */
-void instrumentCoverage(Module *M, Function &F, Instruction &I) {
-  /* Add you code here */
-}
+    std::vector<Type *> Params(3, Type::getInt32Ty(M->getContext()));
+    std::vector<Value *> Args(3);
+    DebugLoc d = I.getDebugLoc();
 
-bool Instrument::runOnFunction(Function &F) {
-  /* Add you code here */
-  return true;
-}
+    IntegerType *i32 = IntegerType::getInt32Ty(M->getContext());
+    ConstantInt *line = ConstantInt::get(i32, d.getLine());
+    ConstantInt *col = ConstantInt::get(i32, d.getCol());
 
-char Instrument::ID = 1;
-static RegisterPass<Instrument>
-    X("Instrument", "Instrumentations for Dynamic Analysis", false, false);
+    Args[0] = I.getOperand(1);
+    Args[1] = line;
+    Args[2] = col;
+
+    FunctionType *FT = FunctionType::get(Type::getVoidTy(M->getContext()), Params, false);
+    FunctionCallee f = M->getOrInsertFunction("__sanitize__", FT);
+    CallInst *Call = CallInst::Create(f, Args, "__sanitize__", &I);
+    Call->setCallingConv(CallingConv::C);
+    Call->setTailCall(true);
+  }
+
+  /*
+   * Implement code coverage instrumentation.
+   */
+  void instrumentCoverage(Module *M, Function &F, Instruction &I)
+  {
+    /* Add you code here */
+    std::vector<Type *> Params(2, Type::getInt32Ty(M->getContext()));
+    std::vector<Value *> Args(2);
+    DebugLoc d = I.getDebugLoc();
+
+    IntegerType *i32 = IntegerType::getInt32Ty(M->getContext());
+    ConstantInt *line = ConstantInt::get(i32, d.getLine());
+    ConstantInt *col = ConstantInt::get(i32, d.getCol());
+
+    Args[0] = line;
+    Args[1] = col;
+
+    FunctionType *FT = FunctionType::get(Type::getVoidTy(M->getContext()), Params, false);
+    FunctionCallee f = M->getOrInsertFunction("__coverage__", FT);
+    CallInst *Call = CallInst::Create(f, Args, "__coverage__", &I);
+    Call->setCallingConv(CallingConv::C);
+    Call->setTailCall(true);
+  }
+
+  bool Instrument::runOnFunction(Function &F)
+  {
+    /* Add you code here */
+    Module *M = F.getParent();
+
+    outs() << "Running DivZeroInstrumentation on " << F.getName() << "\n";
+    for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I)
+    {
+      Instruction &Inst = *I;
+      if (BinaryOperator *BO = dyn_cast<BinaryOperator>(&Inst))
+      {
+        switch (BO->getOpcode())
+        {
+        case Instruction::SDiv:
+        case Instruction::UDiv:
+          outs() << "creating sanitize function\n";
+          instrumentSanitize(M, F, Inst);
+        }
+      }
+
+      instrumentCoverage(M, F, Inst);
+    }
+
+    return true;
+  }
+
+  char Instrument::ID = 1;
+  static RegisterPass<Instrument>
+      X("Instrument", "Instrumentations for Dynamic Analysis", false, false);
 
 } // namespace instrument
